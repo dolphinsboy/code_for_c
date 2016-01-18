f6d93e15bd4770431ae43e4e8910b723d394a44e
dolphinsboy <sguo421@gmail.com>
2016-01-18 15:32:08 +0800

    8-4 演示不同的exit值

--- apue_code/chapter8/8-4.c
+++ apue_code/chapter8/8-4.c
@@ -17,5 +17,23 @@ int main(void)
 
     pr_exit(status);
 
+    if ((pid = fork()) < 0)
+        err_sys("fork error");
+    else if(pid == 0)
+        abort();
+
+    if (wait(&status) != pid)
+        err_sys("wait error");
+    pr_exit(status);
+
+    if ((pid = fork()) < 0)
+        err_sys("fork error");
+    else if(pid == 0)
+        status /= 0;
+
+    if (wait(&status) != pid)
+        err_sys("wait error");
+    pr_exit(status);
+
     return 0;
 }
