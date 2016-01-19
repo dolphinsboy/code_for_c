5e94ab360a1a87a2eb946c725da08332d9bce301
dolphinsboy <sguo421@gmail.com>
2016-01-19 14:50:44 +0800

    8-13 调用system函数

---
+++ apue_code/chapter8/8-13.c
@@ -0,0 +1,16 @@
+#include "apue.h"
+#include "8-3.h"
+#include "8-12.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+    int status;
+
+    if ((status = system("date")) < 0)
+        err_sys("system() error");
+
+    pr_exit(status);
+
+    exit(0);
+}
