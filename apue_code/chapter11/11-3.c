9186632a5c2165a0819aabf1d64ef865e93559d3
dolphinsboy <sguo421@gmail.com>
2016-01-22 14:35:18 +0800

    11-3 pthread_exit参数不正确使用demo的修正版

---
+++ apue_code/chapter11/11-3.c
@@ -0,0 +1,63 @@
+#include "apue.h"
+#include <pthread.h>
+
+struct foo{
+    int a, b, c, d;
+};
+
+void printfoo(const char *s, const struct foo *fp)
+{
+    printf(s);
+    printf(" structure at 0x%x\n", (unsigned)fp);
+    printf(" foo.a = %d\n", fp->a);
+    printf(" foo.b = %d\n", fp->b);
+    printf(" foo.c = %d\n", fp->c);
+    printf(" foo.d = %d\n", fp->d);
+}
+
+void * thr_fn1(void *arg)
+{
+    //struct foo foo = {1, 2, 3, 4};
+    struct foo * fp = calloc(1, sizeof(struct foo));
+    fp->a = 1;
+    fp->b = 2;
+    fp->c = 3;
+    fp->d = 4;
+
+    printfoo("thread 1:\n", fp);
+    pthread_exit((void *)fp);
+}
+
+void *thr_fn2(void *arg)
+{
+    printf("thread 2:\n", pthread_self());
+    pthread_exit((void *)0);
+}
+
+int main(void)
+{
+    int err;
+    pthread_t tid1, tid2;
+    struct foo *fp;
+
+    err = pthread_create(&tid1, NULL, thr_fn1, NULL);
+    if(err != 0)
+        err_quit("can't create thread1:%s\n", strerror(err));
+
+    err = pthread_join(tid1, (void *)&fp);
+    if (err != 0)
+        err_quit("can't join with thread1: %s\n", strerror(err));
+    sleep(1);
+
+    printf("parent starting second thread\n");
+    err = pthread_create(&tid2, NULL, thr_fn2, NULL);
+    if(err != 0)
+        err_quit("can't create thread2:%s\n", strerror(err));
+
+    sleep(1);
+    printfoo("parent:\n", fp);
+
+    if (fp != NULL)
+        free(fp);
+    exit(0);
+}
