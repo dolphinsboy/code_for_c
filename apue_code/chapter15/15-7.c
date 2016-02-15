18cc8c49711512606f4b2685041efc68db8a245e
dolphinsboy <sguo421@gmail.com>
2016-02-15 11:47:48 +0800

    15-7 调用大写/小写过滤程序以读取命令

---
+++ apue_code/chapter15/15-7.c
@@ -0,0 +1,28 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(void)
+{
+    char line[MAXLINE];
+    FILE *fpin;
+
+    if ((fpin = popen("./15-6", "r")) == NULL)
+        err_sys("popen error");
+
+    for (;;){
+        fputs("prompt> ", stdout);
+        fflush(stdout);
+
+        if (fgets(line, MAXLINE, fpin) == NULL)
+            break;
+
+        if(fputs(line, stdout) == EOF)
+            err_sys("fputs error to pipe");
+    }
+
+    if (pclose(fpin) == -1)
+        err_sys("pclose error");
+
+    putchar('\n');
+    exit(0);
+}
